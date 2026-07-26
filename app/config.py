9d2d5bcc45ad5e6ebd9b4c4532a40d49baffb74c
Arsalan Abdul Sattar <arsalanabdulsattar0@gmail.com>
2026-07-26 18:16:41 +0500

    Configure direct secure SMTP password fallback

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     SMTP_SERVER: str = "smtp.gmail.com"
     SMTP_PORT: int = 587
     SMTP_USERNAME: str = "shopnorv@gmail.com"
-    SMTP_PASSWORD: str = ""
+    SMTP_PASSWORD: str = "rgrnncbprxswdzai"
 
     class Config:
         env_file = ".env"
